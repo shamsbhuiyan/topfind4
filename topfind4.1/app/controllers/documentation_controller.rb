a686216b784b80063198cc1c00945b9408e18883
shamsbhuiyan <shamsbhuiyan92@gmail.com>
2015-08-07 14:23:16 -0700

    Finishing models

---
+++ topfind4.1/app/controllers/documentation_controller.rb
@@ -0,0 +1,26 @@
+class DocumentationController < ApplicationController
+ def index
+  	Documentation.show_is(1).order_by(:position)
+  end
+ 
+  def admin
+
+  end
+  
+  def about
+
+  end
+  
+  def license
+
+  end
+  
+  def download
+
+  end
+  
+  
+  def api
+
+  end 
+end
