00b30789f24ea4a6096cca834a8577bc6c48705e
Nik <Nik@dhcp-128-189-224-199.ubcsecure.wireless.ubc.ca>
2016-01-08 13:40:30 -0800

    cleaning up shams mess :-)

--- topfind4.1/c/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '5fb8d119c02b1926733b82b8f78741f153b29dd5e42a0e463c08e18896e26ead2aa4f7d758c7c7086431dba931d7cdc43cd1d4ed6f81305ca8793fb05c8f5186';
--- topfind4.1/s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '7210fa4416143e931cf6d63f5f8f5f97e4b7f72e8b4f864cc7ea6f2938234c6353a50b1dc074543302713bc1d96f7f89dd47553eb606bc6366242a80ad3b90a0';
